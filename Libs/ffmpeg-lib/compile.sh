20fe81c08bfbbd591bded8cef00be6e6b5c16541
unknown <Administrator@.(none)>
2010-04-02 00:39:02 +0800

    added ffmpeg dll version

--- Libs/ffmpeg-lib/compile.sh
+++ Libs/ffmpeg-lib/compile.sh
@@ -2,5 +2,5 @@
 
 #cd ffmpeg
 
-./configure --prefix=../ffmpeg-lib --enable-gpl --enable-postproc --enable-avfilter --enable-avfilter-lavf --enable-w32threads --enable-small --enable-memalign-hack
+./configure --prefix=../ffmpeg-lib --enable-shared --enable-gpl --enable-postproc --enable-avfilter --enable-avfilter-lavf --enable-w32threads --enable-small --enable-memalign-hack
 #--enable-libx264 --enable-libxvid 
