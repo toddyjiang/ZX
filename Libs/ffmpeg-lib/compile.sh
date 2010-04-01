b7c4dfbd6a088a01281e8a7dfdc9223d15d09f45
unknown <Administrator@.(none)>
2010-04-02 00:22:01 +0800

    added ffmpeg svn 22717 2010-03-29

---
+++ Libs/ffmpeg-lib/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#cd ffmpeg
+
+./configure --prefix=../ffmpeg-lib --enable-gpl --enable-postproc --enable-avfilter --enable-avfilter-lavf --enable-w32threads --enable-small --enable-memalign-hack
+#--enable-libx264 --enable-libxvid 
